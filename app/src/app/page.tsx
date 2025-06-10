0e957e7803b9373f48afcc7f7022d2bd4a452baa
noa-okutomi <xokutomi.noa@contract.dentsusoken.com>
2025-06-10 12:02:32 +0900

    test commit v20

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 // const prisma = new PrismaClient();
 
-
 export default async function Home() {
   // const users: User[] = await prisma.user.findMany();
 
@@ -10,7 +9,9 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-        <p className="text-2xl">This is a simple example of a Next.js app with Prisma.</p>
+        <p className="text-2xl">
+          This is a simple example of a Next.js app with Prisma.
+        </p>
         {/* Users:{users.length} */}
         {/* <ul>
           {users.map((user: User) => (
@@ -31,7 +32,7 @@ export default async function Home() {
         >
           Documentation
         </a>
-        </footer>
+      </footer>
     </div>
   );
 }
